801c8f13b3373cde903e36dc5c83ff37d25b7f0d
Amanite <antoine.petit@epita.fr>
2022-12-05 12:15:08 +0100

    TODO: PARSING

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const db = require('./queries')
+
 
 app.use(bodyParser.json())
 app.use(
@@ -14,27 +16,13 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
+
+app.get('/addPoint', db.addPoints)
+app.get('/getPoint', db.getPoint)
+
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
 
-const getPointByDiagram = (request, response) => {
-    const id = parseInt(request.params.id)
-    pool.query('SELECT * FROM points', (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
 
-const addPoints = (request, response) => {
-    const { name, weight_target, target } = request.body
-
-    pool.query('INSERT INTO points (name, weight_target, target) VALUES ($1, $2, $3) RETURNING *', [name, weight_target, target], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(201).send(`User added with ID: ${results.rows[0].id}`)
-    })
-}
--- backend/queries.js
+++ backend/queries.js
@@ -6,3 +6,31 @@ const pool = new Pool({
     password: 'postgres',
     port: 5432,
 })
+
+const getPoint = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query('SELECT * FROM points', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const addPoints = (request, response) => {
+    const { name, weight_target, target } = request.body
+
+    pool.query('INSERT INTO points (name, weight_target, target) VALUES ($1, $2, $3) RETURNING *', [name, weight_target, target], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+    })
+}
+
+
+
+module.exports = {
+    addPoints,
+    getPoint
+}
