a9aec5ee72d046443f5184a28fd15a0c360e784d
Amanite <antoine.petit@epita.fr>
2022-12-05 11:57:51 +0100

    PK

--- database/schema.sql
+++ database/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE point (
     weight_target integer[] NOT NULL,
     id SERIAL NOT NULL,
     name character varying(500) NOT NULL,
+    PRIMARY KEY (id),
     id_diagram integer NOT NULL
 );
