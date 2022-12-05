63af83b863f8217e133c94632fe3f234674d90dc
Amanite <antoine.petit@epita.fr>
2022-12-05 12:11:39 +0100

    Put function

--- backend/index.js
+++ backend/index.js
@@ -18,3 +18,23 @@ app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
 
+const getPointByDiagram = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query('SELECT * FROM points', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const addPoints = (request, response) => {
+    const { name, weight_target, target } = request.body
+
+    pool.query('INSERT INTO points (name, weight_target, target) VALUES ($1, $2, $3) RETURNING *', [name, weight_target, target], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+    })
+}
--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,4 @@
 CREATE TABLE point (
-    value integer NOT NULL,
     target integer[],
     weight_target integer[] NOT NULL,
     id SERIAL NOT NULL,
