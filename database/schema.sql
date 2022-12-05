052d94ec71ae5870629811a9d2706877a9469a1d
Amanite <antoine.petit@epita.fr>
2022-12-05 11:56:49 +0100

    je vous deteste

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE point (
     value integer NOT NULL,
     target integer[],
-    weight_target bigint[] NOT NULL,
+    weight_target integer[] NOT NULL,
     id SERIAL NOT NULL,
     name character varying(500) NOT NULL,
     id_diagram integer NOT NULL
